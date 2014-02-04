fec3efafed50277e3871b3a86bdb059e4c559371
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2014-02-04 14:16:22 -0500

    Past rides can no longer be added. Fixed the form for creating a ride a little bit so if you put in bad information it leaves the correct information (previously it was cutting off second word of start/destination and putting the date in a weird format in both the date and time fields). The form still has some errors if bad input is put in that needs to be fixed.

--- carpool.py
+++ carpool.py
@@ -153,40 +153,45 @@ class Logout(Handler):
 		self.response.headers.add_header('Set-Cookie', str('user=; Path=/'))
 		self.redirect("login")
 class PostRide(Handler):
-	def render_front(self, start="", destination="", startTime="", cost="", passengerMax="", error=""):
-		self.render("postRide.html", start=start, destination=destination, startTime=startTime, cost=cost, passengerMax=passengerMax, error=error)
+	def render_front(self, start="", destination="", startDate="", startTime="", cost="", passengerMax="", error=""):
+		self.render("postRide.html", start=start, destination=destination, startDate=startDate, startTime=startTime, cost=cost, passengerMax=passengerMax, error=error)
 	def get(self):
 		self.checkLogin()
 		self.render_front()
+	#This method needs to be reordered so that it checks for all of the fields being full first thing
 	def post(self):
 		start = self.request.get("start")
 		destination = self.request.get("destination")
-		cost = float(self.request.get("cost"))
-		passengerMax = int(self.request.get("passengerMax"))
+		cost = self.request.get("cost")
+		passengerMax = self.request.get("passengerMax")
+		dateInput = self.request.get("date")#The plain string input
+		timeInput = self.request.get("time")#The plain string input
 		driverId = self.request.cookies.get('user')
 		if driverId:
 			driverId = validation.check_secure_val(driverId)
 			if driverId:
 				driverId = int(driverId)
 		#date time stuff
-		date = map(int, self.request.get("date").split("/",2))
-		timeFull = self.request.get("time").split(" ")
+		date = map(int, dateInput.split("/",2))
+		timeFull = timeInput.split(" ")
 		inputTime = map(int, timeFull[0].split(":"))
 
 		if "PM" in inputTime:
 			inputTime[0] += 12
 
 		startTime = datetime.datetime(date[2],date[0],date[1],inputTime[0],inputTime[1],0)
-
-		if start and destination and startTime and cost and passengerMax:
-			ride = Ride(start=start, destination=destination, startTime=startTime, cost=cost, passengerMax=passengerMax, driverId=driverId, passIds="")
+		if startTime < datetime.datetime.now():
+			error = "You can't create past rides"
+			self.render_front(start, destination, '', '', cost, passengerMax, error)
+		elif start and destination and startTime and cost and passengerMax:
+			ride = Ride(start=start, destination=destination, startTime=startTime, cost=float(cost), passengerMax=int(passengerMax), driverId=driverId, passIds="")
 			ride.put()
 
 			time.sleep(.25) #so that it has time to enter the ride and it appears on home page
 			self.redirect("home")
 		else:
 			error = "Please fill out all the fields!"
-			self.render_front(start, destination, startTime, cost, passengerMax, error)
+			self.render_front(start, destination, dateInput, timeInput, cost, passengerMax, error)
 class Notification(Handler):
 	def writePage(self, error=''):
 		requests = list(db.GqlQuery('SELECT * FROM Request WHERE driverId=:userId', userId=self.getUser()))
