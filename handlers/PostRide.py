8607589a9fbbeb11b3390f9a27cce649297e2c47
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2014-07-29 23:22:38 -0400

    Improved input format verification

--- handlers/PostRide.py
+++ handlers/PostRide.py
@@ -4,6 +4,9 @@ from database import *
 import validation
 import datetime
 import time
+import logging
+import re
+import string
 
 class PostRide(Handler):
 	#Renders the form
@@ -16,17 +19,50 @@ class PostRide(Handler):
 
 	#Posts a new ride for people to join
 	def post(self):
+		#Validates all of the input from the form
+		def validate_inputs(start, destination, cost, passengerMax, dateInput, timeInput):
+			MAXIMUM_SIZE = 8
+			TIME_PATTERN = re.compile(r"^((0?[1-9])|(1[0-2])):[0-5]\d [AP]M$")
+			res = True
+			error = ""
+			if not start:
+				error = "Please enter a starting location."
+				res =  False
+			if not destination:
+				error = "Please enter a destination."
+				res = False
+			if not cost or cost < 0:
+				error = "Please enter a non-negative cost."
+				res = False
+			if not passengerMax or (passengerMax < 1) or (passengerMax > MAXIMUM_SIZE):
+				error = "Please enter a number of passengers between 1 and " + str(MAXIMUM_SIZE)
+				res = False
+			try:
+				date = datetime.datetime.strptime(dateInput, '%m/%d/%Y')
+			except ValueError:
+				error = "Please enter a valid date according to the format MM/DD/YYYY"
+				res = False
+			else:
+				if date < datetime.datetime.now():
+					error = "Please enter a future date."
+					res = False
+			if not TIME_PATTERN.match(timeInput):
+				error = "Please enter the time according to the format HH:MM AM/PM"
+				res = False
+			if error:
+				self.render_front(start, destination, dateInput, timeInput, cost, passengerMax, error)
+			return res
+
 		start = self.request.get("start")
 		destination = self.request.get("destination")
 		cost = self.request.get("cost")
-		passengerMax = self.request.get("passengerMax")
+		passengerMax = int(self.request.get("passengerMax"))
 		dateInput = self.request.get("date")#The plain string input
 		timeInput = self.request.get("time")#The plain string input
 		driverId = self.getUser()
 
-		#date time stuff
-		if start and destination and cost and passengerMax and dateInput and timeInput:
-			date = map(int, dateInput.split("/",2))
+		if validate_inputs(start, destination, cost, passengerMax, dateInput, timeInput):
+			date = map(int, dateInput.split("/", 2))
 			timeFull = timeInput.split(" ")
 			inputTime = map(int, timeFull[0].split(":"))
 
@@ -39,11 +75,8 @@ class PostRide(Handler):
 				error = "You can't create past rides"
 				self.render_front(start, destination, '', '', cost, passengerMax, error)
 			else:
-				ride = Ride(start=start, destination=destination, startTime=startTime, cost=float(cost), passengerMax=int(passengerMax), driverId=driverId, passIds="")
+				ride = Ride(start=start, destination=destination, startTime=startTime, cost=float(cost), passengerMax=passengerMax, driverId=driverId, passIds="")
 				ride.put()
 
 				time.sleep(.25) #so that it has time to enter the ride and it appears on home page
 				self.redirect("home")
-		else:
-			error = "Please fill out all the fields!"
-			self.render_front(start, destination, dateInput, timeInput, cost, passengerMax, error)
