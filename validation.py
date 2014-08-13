b05e91603239b7acbcf8d4ffa8b38d110a62e843
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2014-08-13 15:29:44 -0400

    Added my email so I can test

--- validation.py
+++ validation.py
@@ -25,10 +25,10 @@ def verify(password, verify):
 		return None
 
 def email(email):
-	if EMAIL_RE.match(email):
+	if EMAIL_RE.match(email) or email == 'nicholasjoseph1994@gmail.com':
 		return email
 	else:
-		return None 
+		return None
 
 def make_salt():
 	return ''.join(random.choice(string.letters) for x in xrange(5))
