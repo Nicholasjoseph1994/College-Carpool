0d715babf8f64a712e57edfc7d4a92e14245083f
Sorin Vatasoiu <sorin.vatasoiu@gmail.com>
2014-08-14 19:59:06 -0400

    made driveTime a FloatProperty because it complained. added clientside verification to email by making the email boxes email type

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ class Ride(db.Model):
 	driverId = db.IntegerProperty(required=True)
 	passengerMax = db.IntegerProperty(required=True)
 	cost = db.FloatProperty(required=True)
-	driveTime = db.IntegerProperty(required=True)
+	driveTime = db.FloatProperty(required=True)
 	driveDistance = db.FloatProperty(required=True)
 	passIds = db.StringProperty(required=False)
 	created = db.DateTimeProperty(auto_now_add=True)
