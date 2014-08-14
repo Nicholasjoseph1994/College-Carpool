a707c39eb5ea8065768c04506ef5734b818e8c68
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2014-08-14 16:34:40 -0400

    Fixed Postride so it won't break with invalid geocoding

--- handlers/PostRide.py
+++ handlers/PostRide.py
@@ -25,38 +25,37 @@ class PostRide(Handler):
 			TIME_PATTERN = re.compile(r"^((0?[1-9])|(1[0-2])):[0-5]\d\s*([AP]M|[ap]m)$")
 			res = True
 			error = ''
-			geocodeStart = Geocoder.geocode(start)
-			geocodeDestination = Geocoder.geocode(destination)
 			if not start:
 				error = 'Please enter a starting location.'
 				res =  False
-			if not geocodeStart.valid_address:
+			elif not Geocoder.geocode(start).valid_address:
 				error = 'We could not find your start location. Please check that you spelled it correctly.'
 				res = False
-			if not destination:
+			elif not destination:
 				error = 'Please enter a destination.'
 				res = False
-			if not geocodeDestination.valid_address:
+			elif not Geocoder.geocode(destination).valid_address:
 				error = 'We could not find your destination. Please check that you spelled it correctly.'
 				res = False
-			if not cost or cost < 0:
+			elif not cost or cost < 0:
 				error = 'Please enter a non-negative cost.'
 				res = False
-			if not passengerMax or (passengerMax < 1) or (passengerMax > MAXIMUM_SIZE):
+			elif not passengerMax or (passengerMax < 1) or (passengerMax > MAXIMUM_SIZE):
 				error = 'Please enter a number of passengers between 1 and ' + str(MAXIMUM_SIZE)
 				res = False
-			try:
-				date = datetime.datetime.strptime(dateInput, '%m/%d/%Y')
-			except ValueError:
-				error = 'Please enter a valid date according to the format MM/DD/YYYY'
-				res = False
-			else:
-				if date < datetime.datetime.now():
-					error = 'Please enter a future date.'
-					res = False
-			if not TIME_PATTERN.match(timeInput):
+			elif not TIME_PATTERN.match(timeInput):
 				error = 'Please enter the time according to the format HH:MM AM/PM'
 				res = False
+			else:
+				try:
+					date = datetime.datetime.strptime(dateInput, '%m/%d/%Y')
+				except ValueError:
+					error = 'Please enter a valid date according to the format MM/DD/YYYY'
+					res = False
+				else:
+					if date < datetime.datetime.now():
+						error = 'Please enter a future date.'
+						res = False
 			if error:
 				self.render_front(start, destination, dateInput,
 						timeInput, cost, passengerMax, error)
