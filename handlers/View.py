1e9401eb3ab8cc607455d2ff5c9294e8dc603aad
Nicholas Joseph <nicholasjoseph1994@gmail.com>
2014-08-30 23:00:20 -0400

    Adding sorting by time, cost, start, and destination

--- handlers/View.py
+++ handlers/View.py
@@ -1,59 +1,86 @@
 from google.appengine.ext import db
-from Handler import Handler
-from database import User
-from pygeocoder import Geocoder
 from Handler import Handler, check_login
 from database import User, Ride
+from pygeocoder import Geocoder
 
 class View(Handler):
+	def render_front(self, rides, start="", destination="", error=""):
+		self.render("rideSearch.html", start=start, destination=destination, error=error)
+
 	@check_login
 	def get(self):
+		#Initialization
 		self.deleteOldRides()
 
-		userID = self.getUser()
-		user = User.get_by_id(userID)
+		#Get current user
+		userId = self.getUser()
+		user = User.get_by_id(userId)
 
-		rideIds = user.rideIds
 		#Finding the rides user is not in
+		rideIds = user.rideIds
 		rides = list(Ride.gql("ORDER BY startTime DESC LIMIT 10"))
 		rides = [r for r in rides if r.key().id() not in rideIds]
 
+		#Update ride information
 		for ride in rides:
+			ride.driverName = ride.driver.username
+			ride.seatsLeft = ride.passengerMax - len(ride.passIds)
 
-			ride.driverName = User.get_by_id(ride.driverId).username
+		#Finds the requests the user has made
+		# requests = list(db.GqlQuery("SELECT * FROM Request WHERE requesterId = :userId", userId=self.getUser()))
+		# requests = [x.rideId for x in requests]
+		# rides = [x for x in rides if x.key().id() not in requests]
+		# for ride in rides:
+			# ride.driverName = User.get_by_id(ride.driverId).username
 
 		sortType = self.request.get('sort', default_value='time')
+		raw_start = self.request.get('start')
+		raw_destination = self.request.get('dest')
 		if sortType == 'time':
 			rides = sorted(rides, key=lambda x:x.startTime)
 		elif sortType == 'cost':
 			rides = sorted(rides, key=lambda x:x.cost)
 		elif sortType == 'start':
-			startLocation = Geocoder.geocode(self.request.get('start'))
+			try:
+				start = Geocoder.geocode(raw_start)
+			except:
+				error = 'We could not find your start location. Please check that you spelled it correctly.'
+				self.render_front(rides, rawStart, rawDestination, error)
+				return
 			rides = sorted(rides,
-					key=lambda x:self.getLocationInfo(startLocation, Geocoder.geocode(x.start))['distance']['value'])
+					key=lambda x:self.getLocationInfo(start, Geocoder.geocode(x.start))['distance']['value'])
 		elif sortType == 'dest':
-			destLocation = Geocoder.geocode(self.request.get('dest'))
+			try:
+				destination = Geocoder.geocode(raw_destination)
+			except:
+				error = 'We could not find your destination. Please check that you spelled it correctly.'
+				self.render_front(rides, rawStart, rawDestination, error)
+				return
 			rides = sorted(rides,
-					key=lambda x: self.getLocationInfo(Geocoder.geocode(x), destLocation)['distance']['value'])
+					key=lambda x: self.getLocationInfo(Geocoder.geocode(x.destination), destination)['distance']['value'])
 		elif sortType == 'start_and_dest':
-			startLocation = Geocoder.geocode(self.request.get('start'))
-			destLocation = Geocoder.geocode(self.request.get('dest'))
+			try:
+				start = Geocoder.geocode(raw_start)
+			except:
+				error = 'We could not find your start location. Please check that you spelled it correctly.'
+				self.render_front(rides, rawStart, rawDestination, error)
+				return
+			try:
+				destination = Geocoder.geocode(raw_destination)
+			except:
+				error = 'We could not find your destination. Please check that you spelled it correctly.'
+				self.render_front(rides, rawStart, rawDestination, error)
+				return
+			total_distance = lambda x: self.getLocationInfo(start, Geocoder.geocode(x.start))['distance']['value'] + self.getLocationInfo(Geocoder.geocode(x.destination), destination)['distance']['value']
+
 			rides = sorted(rides,
-					key=lambda x: self.getLocationInfo(startLocation, Geocoder.geocode(x.start))['distance']['value'] +
-					self.getLocationInfo(Geocoder.geocode(x), destLocation)['distance']['value'])
+					key=total_distance)
 		self.render("rideSearch.html", rides=rides)
 	def post(self):
 		process = self.request.get('process')
 		if process == 'more_info':
-			self.redirect('/'+self.request.get('rideId'))
+			self.redirect('/ride/'+self.request.get('rideId'))
 		elif process == 'sort':
-			#keep working here redirect to get request.
 			self.redirect('/view?sort='+self.request.get('searchOptions')
 					+ '&start=' + self.request.get('start')
 					+ '&dest=' + self.request.get('dest'))
-			ride.driverName = ride.driver.username
-			ride.seatsLeft = ride.passengerMax - len(ride.passIds)
-		rides = sorted(rides, key=lambda x:x.startTime)
-		self.render("rideSearch.html", rides=rides)
-	def post(self):
-		self.redirect('/ride/'+self.request.get('rideId'))
